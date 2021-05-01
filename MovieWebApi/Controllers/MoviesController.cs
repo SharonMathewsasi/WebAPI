82c48205eee1e679843f3ed44707b3f6ad82cbeb
SharonMathewsasi <77880370+SharonMathewsasi@users.noreply.github.com>
2021-05-01 09:03:06 +0530

    Add files via upload

---
+++ MovieWebApi/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MmovieWebApi.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        //public string Get()
+        //{
+        // return "Hellofrom WEb API."
+        //}
+
+
+
+        [HttpGet]
+        public List<string> GetMovies()
+        {
+            return new List<string> { "Titanic", "MI", "Matrix" };
+        }
+    }
+}
