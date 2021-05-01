6c4f1c147bf34f43c4f5f2026b04684e47fdffa8
SharonMathewsasi <77880370+SharonMathewsasi@users.noreply.github.com>
2021-05-01 11:41:14 +0530

    Add files via upload

---
+++ LearnTodayAPI/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LearnTodayWebAPI.Models;
+
+namespace LearnTodayWebAPI.Controllers
+{
+    public class AdminController : ApiController
+    {
+        LearnTodayWebAPIDbEntities1 db = new LearnTodayWebAPIDbEntities1();
+        [HttpGet]
+        public IEnumerable<Course> GetAllCourses()
+        {
+            return db.Courses.ToList();
+
+        }
+        public HttpResponseMessage GetCourseById(int id)
+        {
+            var course = db.Courses.Where(c => c.CourseId == id).SingleOrDefault();
+            if (course == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Searched Data not Found");
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, course);
+            }
+
+        }
+    }
+}
---
+++ LearnTodayAPI/Controllers/StudentController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LearnTodayWebAPI.Models;
+
+namespace LearnTodayWebAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        LearnTodayWebAPIDbEntities1 db = new LearnTodayWebAPIDbEntities1();
+
+        [HttpGet]
+        public IEnumerable<Course> GetAllCourses()
+        {
+            return db.Courses.ToList();
+
+        }
+        [HttpPost]
+        public HttpResponseMessage PostStudent([FromBody] Student model)
+        {
+
+            try
+            {
+                db.Students.Add(model);
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, model);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+            }
+
+        }
+        [HttpDelete]
+        public HttpResponseMessage DeleteStudentEnrollment(int id)
+        {
+            var studentEnrollment = db.Students.Where(k => k.StudentId == id).SingleOrDefault();
+            try
+            {
+                if (studentEnrollment != null)
+                {
+                    db.Students.Attach(studentEnrollment);
+                    db.Students.Remove(studentEnrollment);
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, studentEnrollment);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No Enrollment information found");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+            }
+
+        }
+    }
+}
